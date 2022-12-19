44a2b1e44b4e0dba734fdca19f272a59f2ad9527
ncesari49 <cesarin@hotmail.fr>
2022-12-19 15:20:44 +0100

    08

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
 require("dotenv").config();
 
 const express = require("express");
-
 const app = express();
-
 app.use(express.json());
-
 const port = process.env.APP_PORT ?? 5000;
+const movieHandlers = require("./movieHandlers");
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
+const userHandlers = require("./userHandlers");
+
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+const database = require("./database");
+
+const isItDwight = (req, res) => {
+  if (req.body.email === "dwight@theoffice.com" && req.body.password === "123456") {
+    res.send("Credentials are valid");
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users/:id", userHandlers.getUserById);
+app.get("/api/users", userHandlers.getUsers);
+
+app.post("/api/users", hashPassword, userHandlers.postUser);
+app.post("/api/login", userHandlers.getUserByEmail, verifyPassword);
+
+app.use(verifyToken)
+
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+// app.post("/api/login", isItDwight);
+
+
 
 app.listen(port, (err) => {
   if (err) {
@@ -37,3 +54,4 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
--- userHandlers.js
+++ userHandlers.js
@@ -1,7 +1,7 @@
 const database = require("./database");
 
 const getUsers = (req, res) => {
-  const initialSql = "select * from users";
+  const initialSql = "select firstname, lastname, email, city from users";
   const where = [];
 
   if (req.query.city != null) {
@@ -41,7 +41,7 @@ const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from users where id = ?", [id])
+    .query("select firstname, lastname, email, city  from users where id = ?", [id])
     .then(([users]) => {
       if (users[0] != null) {
         res.json(users[0]);
@@ -113,7 +113,31 @@ const deleteUser = (req, res) => {
     });
 };
 
+const getUserByEmail = (req, res, next) => {
+  const { email } = req.body;
+
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+
+
+
 module.exports = {
+  getUserByEmail,
   getUsers,
   getUserById,
   postUser,
